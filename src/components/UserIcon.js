1a51791fbafa685bdabbcfa76f70d58c6c8a308a
Hyphen Frox <yogesh.gautam.infofx@gmail.com>
2021-09-22 04:28:14 +0530

    simplified code

--- src/components/UserIcon.js
+++ src/components/UserIcon.js
@@ -1,50 +1,50 @@
 import React from "react";
 import PersonIcon from "@material-ui/icons/Person";
 import { makeStyles } from "@material-ui/styles";
+import classNames from "classnames";
+
+//
+import useResponsiveSquare from "../services/useResponsiveSquare";
+//
 
 const useStyles = makeStyles({
-  userIconDIV: {
-    position: "relative",
-    width: "100%",
-    "&:after": {
-      content: "''",
-      display: "block",
-      paddingBottom: "100%",
-    },
-  },
   userIconBG: {
-    position: "absolute",
-    width: "100%",
-    height: "100%",
     borderRadius: "50% 50%",
+  },
+  defaultUserIcon: {
+    margin: "auto",
   },
 });
 
 function UserIcon(props) {
   const { userIconURL } = props;
   const userIconStyles = (iconUrl) => {
-    return {
-      backgroundImage: `url(${iconUrl})`,
-      backgroundPosition: "center center",
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-    };
+    if (iconUrl) {
+      return {
+        backgroundImage: `url(${iconUrl})`,
+        backgroundPosition: "center center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      };
+    }
+    return null;
   };
 
   const classes = useStyles();
+  const responsiveSquare = useResponsiveSquare();
 
   if (userIconURL) {
     return (
-      <div className={classes.userIconDIV}>
+      <div className={classNames(responsiveSquare.square)}>
         <div
-          className={classes.userIconBG}
+          className={classNames(responsiveSquare.content, classes.userIconBG)}
           style={userIconStyles(userIconURL)}
         ></div>
       </div>
     );
   }
 
-  return <PersonIcon></PersonIcon>;
+  return <PersonIcon className={classes.defaultUserIcon}></PersonIcon>;
 }
 
 export default UserIcon;
