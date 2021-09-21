23185f51fb2b40cfabc9fce53da503a37465fb62
Hyphen Frox <yogesh.gautam.infofx@gmail.com>
2021-09-22 01:39:16 +0530

    simplified and optimized code created a reusable responsive square style class

--- src/components/ObservationCard.js
+++ src/components/ObservationCard.js
@@ -13,13 +13,17 @@ import VisibilityIcon from "@material-ui/icons/Visibility";
 import PersonIcon from "@material-ui/icons/Person";
 import { useHistory } from "react-router-dom";
 
+//
+import useResponsiveSquare from "../services/useResponsiveSquare";
+import classNames from "classnames";
+//
+
 const useStyles = makeStyles((theme) => ({
   observationCard: (props) => ({
-    width: "100%",
     backgroundColor: "hsla(144, 100%, 50%, 0.3)",
-    cursor: "pointer",
     boxShadow: theme.shadows[4],
     [theme.breakpoints.up(props.maxMobileWidth + 1)]: {
+      cursor: "pointer",
       "&:hover": {
         boxShadow: theme.shadows[10],
       },
@@ -28,23 +32,6 @@ const useStyles = makeStyles((theme) => ({
       marginTop: "0.5em",
     },
   }),
-  observationCommonName: {
-    marginTop: "1em",
-  },
-  observationSpeciesName: {
-    fontSize: "0.9rem",
-    fontStyle: "italic",
-  },
-  observationLocationInfoBox: {
-    display: "flex",
-    alignItems: "center",
-    "& > *": {
-      margin: "0.1em",
-    },
-  },
-  observationLocation: {
-    fontSize: "0.85rem",
-  },
   userAndTimeInfoBox: {
     marginTop: "0.5em",
     display: "flex",
@@ -53,43 +40,15 @@ const useStyles = makeStyles((theme) => ({
       margin: "0.25em",
     },
   },
-  userInfoBox: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    "& > * + *": {
-      marginLeft: "0.5em",
-    },
-  },
   userIcon: {
     width: "40px",
     height: "40px",
     borderRadius: "50% 50%",
   },
-  userName: {
-    fontSize: "1rem",
-  },
-  createdAtTimeBox: {
+  iconAndTextBox: {
     display: "flex",
-    justifyContent: "center",
     alignItems: "center",
-    "& > * + *": {
-      marginLeft: "0.5em",
-    },
-  },
-  createdAtTime: {
-    fontSize: "1rem",
-  },
-  observedOnTimeBox: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    "& > * + *": {
-      marginLeft: "0.5em",
-    },
-  },
-  observedOnTime: {
-    fontSize: "1rem",
+    gap: "0.5em",
   },
   moreDetailsButton: {
     marginLeft: "auto",
@@ -116,27 +75,16 @@ function ObservationCard(props) {
 
   //observation photo styles
   const observationPhotoStyles = (url) => {
-    const isPhotoAvailable = typeof url === "string";
-    if (isPhotoAvailable) {
+    if (url) {
       url = url.replace("square", "original");
       const styles = {
-        width: "100%",
-        height: "0",
-        paddingTop: "100%",
+        backgroundImage: `url(${url})`,
+        backgroundSize: "cover",
         backgroundPosition: "center center",
         backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundImage: `url(${url})`,
       };
       return styles;
-    } else
-      return {
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      };
+    } else return null;
   };
   //
 
@@ -152,48 +100,69 @@ function ObservationCard(props) {
   //
 
   const classes = useStyles({ maxMobileWidth });
+  const responsiveSquare = useResponsiveSquare();
 
   return (
     <Card
       className={classes.observationCard}
-      onClick={handleCardClick(observationData.id)}
+      onClick={!isMobile ? handleCardClick(observationData.id) : null}
       {...args}
     >
       <CardContent>
         <div
+          className={responsiveSquare.square}
           style={observationPhotoStyles(
             observationData?.observation_photos[0]?.photo?.url
           )}
         >
           {observationData?.observation_photos[0]?.photo?.url ? null : (
-            <Typography variant="subtitle2">No Photo Available</Typography>
+            <div
+              className={responsiveSquare.content}
+              style={{
+                display: "grid",
+                gridTemplateColumns: "100%",
+                alignContent: "center",
+              }}
+            >
+              <Typography variant="subtitle2" align="center">
+                No Photo Available
+              </Typography>
+            </div>
           )}
         </div>
         <Typography
           className={classes.observationCommonName}
           variant="subtitle1"
+          style={{ marginTop: "1em" }}
         >
           {observationData?.taxon?.preferred_common_name ?? "Common Name N/A"}
         </Typography>
         <Typography
           className={classes.observationSpeciesName}
           variant="subtitle1"
+          style={{
+            fontSize: "0.9rem",
+            fontStyle: "italic",
+          }}
         >
           {`${observationData?.species_guess ?? "Unkown Scientific Name"}, ${
             observationData?.taxon?.iconic_taxon_name ?? "Unknown Kind"
           }`}
         </Typography>
-        <div className={classes.observationLocationInfoBox}>
+        <div className={classes.iconAndTextBox}>
           <LocationOnIcon></LocationOnIcon>
           <Typography
             className={classes.observationLocation}
             variant="subtitle1"
+            style={{
+              fontSize: "0.85rem",
+            }}
           >
             {observationData?.place_guess ?? "Location N/A"}
           </Typography>
         </div>
         <div className={classes.userAndTimeInfoBox}>
-          <div className={classes.userInfoBox}>
+          <div className={classes.iconAndTextBox}>
             {observationData?.user?.icon_url ? (
               <div
                 className={classes.userIcon}
@@ -202,21 +171,39 @@ function ObservationCard(props) {
             ) : (
               <PersonIcon></PersonIcon>
             )}
-            <Typography variant="subtitle1" className={classes.userName}>
+            <Typography
+              variant="subtitle1"
+              className={classes.userName}
+              style={{
+                fontSize: "1rem",
+              }}
+            >
               {observationData?.user?.name ?? "Unknown Observer"}
             </Typography>
           </div>
-          <div className={classes.observedOnTimeBox}>
+          <div className={classes.iconAndTextBox}>
             <VisibilityIcon></VisibilityIcon>
-            <Typography variant="subtitle1" className={classes.observedOnTime}>
+            <Typography
+              variant="subtitle1"
+              className={classes.observedOnTime}
+              style={{
+                fontSize: "1rem",
+              }}
+            >
               {observationData?.time_observed_at
                 ? time_observed_at.toLocaleString()
                 : "Observation Date Unknown"}
             </Typography>
           </div>
-          <div className={classes.createdAtTimeBox}>
+          <div className={classes.iconAndTextBox}>
             <CreateIcon></CreateIcon>
-            <Typography variant="subtitle1" className={classes.createdAtTime}>
+            <Typography
+              variant="subtitle1"
+              className={classes.createdAtTime}
+              style={{
+                fontSize: "1rem",
+              }}
+            >
               {observationData?.created_at
                 ? created_at.toLocaleString()
                 : "Creation Date Unknown"}
