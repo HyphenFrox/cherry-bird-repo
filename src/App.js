ddd7f3d0f85b472ded29fdb24bac27c73d5b750c
Hyphen Frox <yogesh.gautam.infofx@gmail.com>
2021-09-21 21:37:44 +0530

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from "react"
-// import { Suspense } from "react";
+import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -9,9 +8,6 @@ import "./styles.css";
 import Theme from "./components/Theme";
 import Homepage from "./pages/Homepage";
 import Observation from "./pages/Observation";
-// import GlobalProgressLoader from "./components/GlobalProgressLoader";
-
-// const Test = React.lazy(() => import("./Test"));
 //
 
 const MyQueryClient = new QueryClient();
@@ -19,7 +15,6 @@ const MyQueryClient = new QueryClient();
 function App() {
   return (
     <>
-      {/* <Suspense fallback={<GlobalProgressLoader></GlobalProgressLoader>}> */}
       <Theme>
         <QueryClientProvider client={MyQueryClient}>
           <Router>
@@ -30,15 +25,11 @@ function App() {
               <Route path="/observation/:observationID" exact>
                 <Observation />
               </Route>
-              {/* <Route path="/test" exact>
-                  <Test></Test>
-                </Route> */}
             </Switch>
           </Router>
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </Theme>
-      {/* </Suspense> */}
     </>
   );
 }
