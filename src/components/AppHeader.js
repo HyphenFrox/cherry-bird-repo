813d3426ddc2adf248944338d0069ad6d6cd5578
Hyphen Frox <yogesh.gautam.infofx@gmail.com>
2021-09-24 03:48:57 +0530

    added react-router intra-app routing

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -2,18 +2,22 @@ import React from "react";
 import { AppBar, Link, Toolbar } from "@material-ui/core";
 import { GitHub } from "@material-ui/icons";
 
+//
+import CustomRouterLink from "./CustomRouterLink";
+//
+
 function AppHeader() {
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
-        <Link
+        <CustomRouterLink
+          to={"/"}
           variant="h1"
-          href="/"
           underline="none"
           style={{ fontSize: "2rem", color: "white" }}
         >
           Cherry Bird Repo
-        </Link>
+        </CustomRouterLink>
         <div style={{ flexGrow: 1 }}></div>
         <Link
           href="https://github.com/HyphenFrox/cherry-bird-repo"
