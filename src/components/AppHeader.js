25a286c83c4f4e33c073389793e7f633fd2b7bc7
Hyphen Frox <yogesh.gautam.infofx@gmail.com>
2021-09-24 06:12:14 +0530

    removed custom router link component

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,23 +1,21 @@
 import React from "react";
 import { AppBar, Link, Toolbar } from "@material-ui/core";
 import { GitHub } from "@material-ui/icons";
-
-//
-import CustomRouterLink from "./CustomRouterLink";
-//
+import { Link as RouterLink } from "react-router-dom";
 
 function AppHeader() {
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
-        <CustomRouterLink
-          to={"/"}
-          variant="h1"
+        <Link
+          component={RouterLink}
+          to="/"
           underline="none"
+          variant="h1"
           style={{ fontSize: "2rem", color: "white" }}
         >
           Cherry Bird Repo
-        </CustomRouterLink>
+        </Link>
         <div style={{ flexGrow: 1 }}></div>
         <Link
           href="https://github.com/HyphenFrox/cherry-bird-repo"
--- src/components/CustomRouterLink.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Link } from "@material-ui/core";
-import { Link as RouterLink } from "react-router-dom";
-
-const CustomRouterLink = React.forwardRef((props, ref) => (
-  <Link ref={ref} component={RouterLink} {...props}></Link>
-));
-
-export default CustomRouterLink;
